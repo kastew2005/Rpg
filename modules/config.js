69f98708cb0c8ef34b6b2a76d4885a0f0b44dda6
kastew2005 <kastew2911@icloud.com>
2026-08-30 14:40:12 +0300

    Create config.js

---
+++ modules/config.js
@@ -0,0 +1,17 @@
+export const CONFIG = {
+    playerSpeed: 3,
+    npcSpeed: 0.5,
+    tileSize: 32,
+    worldWidth: 1200,
+    worldHeight: 1200,
+    colors: {
+        grass: '#4a8f3f',
+        grassLight: '#5ca84f',
+        dirt: '#8b7d6b',
+        path: '#b8a58a',
+        water: '#4a7faf',
+        roof: '#8b3a3a',
+        wall: '#d4b896',
+        shadow: 'rgba(0,0,0,0.25)'
+    }
+};
